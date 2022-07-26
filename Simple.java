6b602338c5dfc94c665d8e7c6beebb21164f4a57
DevRLama <dev.lama007@gmail.com>
2022-07-26 11:32:51 +0530

    Simple changed

--- Simple.java
+++ Simple.java
@@ -1,11 +1,11 @@
 class Simple
 {
-    Static int m=100;
+    static int m=100;
     void method()
     {
         int n=90;
     }
-    public Static void main(String args[])
+    public static void main(String args[])
     {
         int data=50;
     }
