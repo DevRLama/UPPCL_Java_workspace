264f17641b582a4aeaa938820fa525c6b3297c75
DevRLama <dev.lama007@gmail.com>
2022-07-26 11:04:21 +0530

    Java_workspace

---
+++ Simple.java
@@ -0,0 +1,12 @@
+class Simple
+{
+    Static int m=100;
+    void method()
+    {
+        int n=90;
+    }
+    public Static void main(String args[])
+    {
+        int data=50;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,11 @@
+class Student
+{
+    int id;
+    String name;
+    public static void main(String args[])
+    {
+        Student s1=new Student();   // Initialisation of object with the help of new keyword.
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
