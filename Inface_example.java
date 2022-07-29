47d01c73cc457d1d83384c09594a0eef383e7411
DevRLama <dev.lama007@gmail.com>
2022-07-29 12:56:08 +0530

    29-07-2022 12:55 Timestamp

---
+++ Inface_example.java
@@ -0,0 +1,41 @@
+// Interface
+interface Animal {
+    public void animalSound(); // interface method (does not have a body)
+    public void sleep(); // interface method (does not have a body)
+  }
+  
+  // Pig "implements" the Animal interface
+  class Pig implements Animal {
+    public void animalSound() {
+      // The body of animalSound() is provided here
+      System.out.println("The pig says: wee wee");
+    }
+    public void sleep() {
+      // The body of sleep() is provided here
+      System.out.println("Zzz");
+    }
+  }
+
+  class Dog implements Animal{
+    public void animalSound(){
+        System.out.println("The sound is Boo Boo");
+    }
+
+    public void sleep(){
+        System.out.println("The dog sleeping Zzzz");
+    }
+
+
+  }
+  
+  public class Inface_example {
+    public static void main(String[] args) {
+      Pig myPig = new Pig();  // Create a Pig object
+      Dog myDog = new  Dog();
+      myPig.animalSound();
+      myPig.sleep();
+      myDog.animalSound();
+      myDog.sleep();
+    }
+  }
+
